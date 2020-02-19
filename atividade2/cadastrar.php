a3d6db0f3f9f0e0a9c0f01b24f49f06705f64195
Vinicius Schulte <schultev2@gmail.com>
2020-02-19 09:30:00 -0300

    Criado stmt para o mysql

--- atividade2/cadastrar.php
+++ atividade2/cadastrar.php
@@ -7,11 +7,19 @@
     $db_password = "coringa";
 
     $con = new PDO("mysql:host=$host; dbname=$db", $db_user, $db_password);
-    
 
-    if($con) {
-        echo "Conectado!";
-    }
+
+    $stmt = $con->prepare("INSERT INTO users(nome, cpf, rg, endereco, email, senha, data_nascimento) 
+                            VALUES(:nome, :cpf, :rg, :endereco, :email, :senha, :data_nascimento)");
+    $stmt->bindParam(':nome', $user['nome_completo']);
+    $stmt->bindParam(':cpf', $user['cpf']);
+    $stmt->bindParam(':rg', $user['rg']);
+    $stmt->bindParam(':endereco', $user['endereco']);
+    $stmt->bindParam(':email', $user['email']);
+    $stmt->bindParam(':senha', $user['password']);
+    $stmt->bindParam(':data_nascimento', $user['data_nascimento']);
+
+    $stmt->execute();
 ?>
 
 <!DOCTYPE html>
