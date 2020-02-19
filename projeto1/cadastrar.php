7c7076378df88e02f9e9f8adc7d55e77443cff50
Vinicius Schulte <schultev2@gmail.com>
2020-02-19 00:16:39 -0300

    Formulario terminado

--- projeto1/cadastrar.php
+++ projeto1/cadastrar.php
@@ -6,4 +6,26 @@
     $sobrenome = $_REQUEST['sobrenome'];
     $salario = $_REQUEST['salario'];
     $numCasa = $_REQUEST['numCasa'];
+
+    echo "
+    <!DOCTYPE html>
+    <html lang='pt-BR'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Document</title>
+    </head>
+    <body>
+        <div style='display:flex; flex-direction:column;'
+            <span>email: {$email}</span>
+            <span>senha: {$senha}</span>
+            <span>telefone: {$telefone}</span>
+            <span>nome: {$nome}</span>
+            <span>sobrenome: {$sobrenome}</span>
+            <span>salario: {$salario}</span>
+            <span>numCasa: {$numCasa}</span>
+        </div>
+    </body>
+    </html>  
+    ";
 ?>
